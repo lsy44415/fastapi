8698456971c400cf2b51949473f11e27bad43148
Siyi Liu <30919966+lsy44415@users.noreply.github.com>
2024-05-24 21:09:38 +1000

    remove personal mongoDB link

--- app/server/database.py
+++ app/server/database.py
@@ -1,8 +1,8 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 
-MONGO_DETAILS = "mongodb+srv://lsy44415:Lsy4986881@cluster0.vls4aqs.mongodb.net/"
-#MONGO_DETAILS = "mongodb://localhost:27017"
+
+MONGO_DETAILS = "mongodb://localhost:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
 database = client.bcis
